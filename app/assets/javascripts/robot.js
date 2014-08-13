028eac80b032a9230fa942740a506f91d6877e2f
GeorgeSomerville1992 <georgesomerville1992@gmail.com>
2014-08-13 20:43:48 +0100

    first commit

---
+++ app/assets/javascripts/robot.js
@@ -0,0 +1,13 @@
+// robot postion conssits of a grid corrdinate  - x cordinate followed by cordinate
+// and orinatation so N, S, E, W 
+
+// A robot instruction is a string of the letters “L”, “R”, and “F” which represent, respectively, the
+// instructions:
+// ● Left : the robot turns left 90 degrees and remains on the current grid point.
+// ● Right : the robot turns right 90 degrees and remains on the current grid point.
+// ● Forward : the robot moves forward one grid point in the direction of the current
+// orientation and maintains the same orientation.
+
+
+// The direction North corresponds to the direction from grid point (x, y) to grid point (x, y+1).
+
